c5e084805705056dda8944a1abe10f56ea02fc66
Pranshu Verma <er.pranshuverma68@gmail.com>
2025-11-13 10:52:12 +0000

    comparison

---
+++ JavaScript_Practise/datatypessummary.js
@@ -0,0 +1,38 @@
+// Primitive
+// 7 types : string  , Number , boolean , null, undefined, Symbol , BigInt 
+
+//Reference (Now primitive)
+ // array , Objects , functions
+
+// java scipt is a dynamically typed languague
+
+const scoreValue = 99.99
+const isLoggedIn = false;
+ let useEmail = " undefined"
+
+  const  id = Symbol('123')
+
+  const anotherId = Symbol('123')
+  console.log(id === anotherId);
+
+  const bigNumber = 9999999999999999999999999999999999n;
+  console.log(bigNumber);
+
+  const heroes = ['shakiman' ," nagraj"];
+  console.log(heroes);
+
+  const myObj = {
+    name : "pranshu ",
+    age : 45,
+
+  }
+  console.log(myObj);
+  
+ const  myFunction = function (){
+    console.log('hello world');
+    }
+  
+    console.log(myFunction());
+    
+  
+  
