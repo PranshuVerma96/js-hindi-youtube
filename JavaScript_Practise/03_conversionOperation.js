c8e89f758cf8e7fd10e6c9f4ded0e0f789046709
Pranshu Verma <er.pranshuverma68@gmail.com>
2025-11-13 10:25:38 +0000

    conversion of datatypes

---
+++ JavaScript_Practise/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "99ba";
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+// "99" = 99
+// "99p" = NaN
+// true = 1; false = 0
+ let isLoggedIn = "";
+ let booleanIsLoggedIN = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIN);
+
+ // 1  = true  0 = false
+ // "" = false
+ // 'pransh = true 
+
+ let str = 33
+  let stringNumber = String(str)
+  console.log(stringNumber);
+  
