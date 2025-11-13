7ae0bb54d1d8d9fba5796e79d431086a48a7c3c4
Pranshu Verma <er.pranshuverma68@gmail.com>
2025-11-13 10:05:06 +0000

    hello coder

---
+++ JavaScript_Practise/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144;
+let accountEmail = "pransh@google.com";
+var accountPassword = '9999';
+// accountCity = 'rajsthan';
+let accountState;
+console.log(accountState);
+
+// if you can not instilize any variable and can not assian any 
+// value than javascript consider in "Undefined value"
+
+
+// // /accountId = 2;
+//  console.log(accountId);
+
+//  accountEmail = 'hcAgmail.com';
+//  console.log(accountEmail);
+//  console.table(accountId,accountEmail,accountPassword);
+ 
+ console.log(accountEmail);
+
+ /*
+ Prefer not to use var 
+ beacuse of issue in block scope and functional scope
+ */
+ 
