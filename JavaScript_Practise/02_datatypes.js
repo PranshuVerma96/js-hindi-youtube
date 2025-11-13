59ad75192a0037701c47ea02c2ec8a6c90bab275
Pranshu Verma <er.pranshuverma68@gmail.com>
2025-11-13 10:16:08 +0000

    datatypes

---
+++ JavaScript_Practise/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict "; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs not brower
+ let name = 'hitesh';
+ let age = 18;
+ let isLoggedIn = false
+ // null = standalone value
+
+ let state = null
+ console.log(state);
+
+ // symbol = unique
+// object 
+
+console.log(typeof(age));
+console.log(typeof(state));// object
+console.log(typeof(undefined)); // undefined
+
+
+
+ 
+
