77afa2f9d42c629f64f5804ca4451080920f7479
Pranshu Verma <er.pranshuverma68@gmail.com>
2025-11-14 10:34:59 +0000

    math and num

---
+++ JavaScript_Practise/nums_in_math.js
@@ -0,0 +1,56 @@
+const score = 900
+const scoreCard = new Number(100);
+console.log(score);
+// console.log(scoreCard);
+
+
+// console.log(score == scoreCard);
+
+//console.log(scoreCard.toString().length);
+
+// to Fixed method are in when you created the ecommerce and pay card
+
+//console.log(scoreCard.toFixed(2)); 
+
+const otherNumber = 34.255553;
+
+//console.log(otherNumber.toPrecision(2));
+
+const hundereds = 1000000
+console.log(hundereds.toLocaleString('en-IN'));
+
+
+
+
+
+
+
+
+///// +++++++++++++++++++++++ Maths +++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+
+// console.log(Math.round(4.93));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(8.9));
+
+// console.log(Math.min(1,2,3,4));
+console.log(Math.round((Math.random()*10)+1));
+
+const min = 10
+const max = 80
+console.log((Math.random()*(max-min + 1)));
+
+
+
+
+
+
+
+
+
+
+
+
+
