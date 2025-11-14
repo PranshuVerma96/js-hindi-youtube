e63af3fcd5bc174142a81eb6b8d5e076c64c0b48
Pranshu Verma <er.pranshuverma68@gmail.com>
2025-11-14 09:58:19 +0000

    string in javascript

---
+++ JavaScript_Practise/string.js
@@ -0,0 +1,51 @@
+const name = "hitesh";
+const repoCount = 89;
+
+// console.log(name + repoCount + "value");
+
+console.log(`hello my name is ${name } and my repo is ${repoCount}`);
+
+const gameName = new String('hitesh-hc');
+console.log(gameName);
+console.log(typeof(gameName));
+
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toLocaleLowerCase());
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('t')
+
+const newString = gameName.substring(0,4)
+
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4);
+console.log(anotherString);
+
+const newString1 = "         pranshu   "
+console.log(newString1);
+console.log(newString1.trim());
+
+
+const url = "https://hiesh.com/pranshuverma";
+console.log(url.replace('pranshuverma' ,'sandesh'));
+console.log(url);
+
+console.log(url.includes('hi'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
