e1e628ad70ff72c055d925f7d2c0f3d2a4f443f6
Pranshu Verma <er.pranshuverma68@gmail.com>
2025-11-15 11:07:23 +0000

    obectdeStructure and SomeApi and json

---
+++ JavaScript_Practise/Object/objectDestructure.js
@@ -0,0 +1,35 @@
+// object destructuring
+
+const course = {
+    coursename : 'javascript',
+    price  : 99,
+    courseInstrucor : 'hitesh'
+
+}
+
+const  {courseInstrucor : instructor} = course
+// console.log(courseInstrucor);
+console.log(instructor);
+
+
+
+// const navbar = ({company}) =>{
+
+// }
+
+// navbar(company = "hitesh")
+// console.log(company);
+
+// api 
+
+// {
+//     "name" : 'hitesh',
+//     "coursename" : 'js',
+//     "price" : 'free'
+
+// }
+
+[
+    {},
+    {},
+]
