98b5d72116261c9add873bb8e0e7ad1923abfb81
Pranshu Verma <er.pranshuverma68@gmail.com>
2025-11-15 11:53:58 +0000

    function with array and object

---
+++ JavaScript_Practise/Function/02_function.js
@@ -0,0 +1,24 @@
+function calculateCarPrice(...num1){
+    return num1;
+}
+// console.log(calculateCarPrice(3, 4,5 , 9 , 8));
+
+const user ={
+    username  :'hitesh',
+    prices :90
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+
+handleObject(user);
+
+const myNewArray = [1,2,3,4];
+function returnSecondValue(getarray){
+    return getarray[0]
+}
+console.log(myNewArray);
+console.log(returnSecondValue(myNewArray));
+
